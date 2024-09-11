290af213d9e759bfd9723fe2a9a91fb4654ba706
EmitoStuer <stuer.87@gmail.com>
2024-09-11 20:44:14 -0300

    Cargando Api pokemon

---
+++ script.js
@@ -0,0 +1,25 @@
+const contenedor = document.getElementById("contenedor")
+fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0")
+.then(resp => resp.json())
+.then (data => {
+    //console.log(data)
+    contenedor.innerHTML = "<h1>Pokemon</h1>"
+    data.results.forEach(pokemon => {
+            fetch(pokemon.url)
+            .then(resp => resp.json())
+            .then(pokemon => {
+                //console.log(pokemon.sprites.other)
+                contenedor.innerHTML += `<div class="card">
+                                    <h3>#${pokemon.id} ${pokemon.name} </h3>
+                                    <h4>Altura: ${pokemon.height} Pies </h4>
+                                    <h4>Peso: ${pokemon.weight} Libras </h4>
+                                    <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}">
+                                    
+                                </div>`
+            })
+    });
+}).catch(error => console.log(error))
+
+//<img src="${pokemon.sprites.other.showdown.front_default}" alt="${pokemon.name}">
+
+//<img src="${pokemon.sprites.other.official-artwork.front_default}" alt="${pokemon.name}">
